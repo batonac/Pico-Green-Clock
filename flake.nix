b8859bd9d1a48ee97b7ff23340cf0659795fce6e
Kevin Shenk <kevin@avu.nu>
2025-08-25 11:18:29 -0400

    update flake + add build log

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     
     pico-sdk = {
-      url = "git+https://github.com/raspberrypi/pico-sdk?ref=refs/tags/2.0.0&submodules=1";
+      url = "git+https://github.com/raspberrypi/pico-sdk?ref=refs/tags/2.1.0&submodules=1";
       flake = false;
     };
 
@@ -40,18 +40,6 @@
         git
       ];
 
-      # Common CMake flags
-      commonCMakeFlags = [
-        "-DPICO_SDK_PATH=${pico-sdk}"
-        "-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
-        "-DCMAKE_SYSTEM_NAME=Generic"
-        "-DCMAKE_C_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
-        "-DCMAKE_CXX_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++"
-        "-DCMAKE_ASM_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
-        "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
-        "-DFETCHCONTENT_SOURCE_DIR_PICOTOOL=${picotool}"
-      ];
-
       # Function to create a build derivation
       mkPicoGreenClock = { variant, enableWifi ? false }: pkgs.stdenv.mkDerivation {
         pname = "pico-green-clock-${variant}";
@@ -61,9 +49,20 @@
 
         nativeBuildInputs = commonBuildInputs;
 
-        cmakeFlags = commonCMakeFlags ++ [
+        cmakeFlags = [
+          "-DPICO_SDK_PATH=${pico-sdk}"
+          "-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
+          "-DCMAKE_SYSTEM_NAME=Generic"
+          "-DCMAKE_C_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
+          "-DCMAKE_CXX_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++"
+          "-DCMAKE_ASM_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
           (if enableWifi then "-DPICO_BOARD=pico_w" else "-DPICO_BOARD=pico")
-        ];
+        ] ++ (if enableWifi then [
+          "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
+          "-DFETCHCONTENT_SOURCE_DIR_PICOTOOL=${picotool}"
+        ] else [
+          "-DPICO_NO_PICOTOOL=ON"
+        ]);
 
         # Set required environment variables
         PICO_SDK_PATH = "${pico-sdk}";
@@ -75,8 +74,31 @@
           # Create symbolic link for pico_sdk_import.cmake
           ln -sf ${pico-sdk}/external/pico_sdk_import.cmake pico_sdk_import.cmake
           
-          # Use the appropriate CMakeLists.txt based on variant
-          cp ${if enableWifi then "CMakeLists.txt.PicoW" else "CMakeLists.txt.Pico"} CMakeLists.txt
+          # Use the existing CMakeLists.txt and modify it for the variant
+          ${if enableWifi then ''
+            # For Pico W, use CMakeLists.txt as-is (it's already configured for Pico W)
+            echo "Using existing CMakeLists.txt for Pico W"
+          '' else ''
+            # For regular Pico, modify CMakeLists.txt to remove WiFi dependencies
+            sed -i 's/set(PICO_BOARD pico_w CACHE STRING "Board type")/set(PICO_BOARD pico CACHE STRING "Board type")/' CMakeLists.txt
+            # Remove WiFi-specific source files from add_executable line only
+            sed -i 's/picow_ntp_client.c//' CMakeLists.txt
+            sed -i 's/ssi.c cgi.c//' CMakeLists.txt
+            # Remove WiFi-specific libraries from target_link_libraries
+            sed -i '/pico_cyw43_arch_lwip_threadsafe_background/d' CMakeLists.txt
+            sed -i '/pico_lwip_http/d' CMakeLists.txt
+            # Remove the WiFi include directory
+            sed -i '/pico_cyw43_arch\/include/d' CMakeLists.txt
+            # Remove WiFi-related includes from the main C file
+            sed -i '/#include.*picow_ntp_client.h/d' Pico-Green-Clock.c
+            # Comment out the makefsdata script execution since it's WiFi-related
+            sed -i '/message.*makefsdata/,/^)$/s/^/# /' CMakeLists.txt
+            # Create empty html_files directory and htmldata.c to prevent missing files
+            mkdir -p html_files
+            touch htmldata.c
+            # Comment out pico_add_extra_outputs to avoid picotool issues
+            sed -i 's/pico_add_extra_outputs/# pico_add_extra_outputs/' CMakeLists.txt
+          ''}
 
           # Configure with cmake
           cmake -B build -S . ''${cmakeFlagsArray[@]} ''${cmakeFlags[@]} 
@@ -96,7 +118,11 @@
           runHook preInstall
           
           mkdir -p $out/bin
-          cp build/Pico-Green-Clock.{elf,uf2} $out/bin/${variant}.{elf,uf2}
+          cp build/Pico-Green-Clock.elf $out/bin/${variant}.elf
+          # Copy UF2 file if it exists (might not exist for regular Pico build)
+          if [ -f build/Pico-Green-Clock.uf2 ]; then
+            cp build/Pico-Green-Clock.uf2 $out/bin/${variant}.uf2
+          fi
           
           runHook postInstall
         '';
@@ -138,9 +164,8 @@
           git
           
           # Additional development tools
-          gdb-multiarch
+          gdb
           minicom
-          picotool
         ];
 
         shellHook = ''
